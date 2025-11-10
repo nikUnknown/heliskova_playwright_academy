48eca08a7c6c1abac9efb45660a894c1166cbaf5
Nikola Heliskova <he.ni@seznam.cz>
2025-11-10 17:25:14 +0100

    Playwright - create first test, modify actions config to run only 1 test

---
+++ tests/learning/first-tests/first.spec.ts
@@ -0,0 +1,10 @@
+import { test } from "@playwright/test";
+
+// ? Snippet: pwt (pw-test)
+test("First Test", async ({ page }) => {
+  // ? testovaci kroky
+  await page.goto("https://tredgate.com/pmtool");
+  await page.locator("#username").fill("pw_academy");
+  await page.locator("#password").fill("Playwright321!");
+  await page.locator('[type="submit"]').click();
+});
