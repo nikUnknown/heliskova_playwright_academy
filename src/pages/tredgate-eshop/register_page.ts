32fecbd739068e6b72f8dc71607351d090610558
Nikola Heliskova <he.ni@seznam.cz>
2025-11-14 19:38:25 +0100

    HW - page objects - register new account

---
+++ src/pages/tredgate-eshop/register_page.ts
@@ -0,0 +1,57 @@
+import { Locator, Page } from "@playwright/test";
+
+export class RegisterPage {
+  readonly page: Page;
+  readonly firstNameInput: Locator;
+  readonly lastNameInput: Locator;
+  readonly emailInput: Locator;
+  readonly phoneInput: Locator;
+  readonly passwordInput: Locator;
+  readonly passwordConfirmInput: Locator;
+  readonly privacyPolicyCheck: Locator;
+  readonly continueButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.firstNameInput = page.locator("#input-firstname");
+    this.lastNameInput = page.locator("#input-lastname");
+    this.emailInput = page.locator("#input-email");
+    this.phoneInput = page.locator("#input-telephone");
+    this.passwordInput = page.locator("#input-password");
+    this.passwordConfirmInput = page.locator("#input-confirm");
+    this.privacyPolicyCheck = page.locator('input[name="agree"]');
+    this.continueButton = page.locator('input[type="submit"]');
+  }
+
+  async fillFirstName(firstNameInput: string) {
+    await this.firstNameInput.fill(firstNameInput);
+  }
+
+  async fillLastName(lastNameInput: string) {
+    await this.lastNameInput.fill(lastNameInput);
+  }
+
+  async fillEmail(emailInput: string) {
+    await this.emailInput.fill(emailInput);
+  }
+
+  async fillPhone(phoneInput: string) {
+    await this.phoneInput.fill(phoneInput);
+  }
+
+  async fillPassword(passwordInput: string) {
+    await this.passwordInput.fill(passwordInput);
+  }
+
+  async fillPasswordConfirmation(passwordConfirmInput: string) {
+    await this.passwordConfirmInput.fill(passwordConfirmInput);
+  }
+
+  async clickPrivacyPolicy() {
+    await this.privacyPolicyCheck.check();
+  }
+
+  async clickToRegister() {
+    await this.continueButton.click();
+  }
+}
---
+++ tests/homeworks/homework_page_objects.spec.ts
@@ -0,0 +1,20 @@
+import { test } from "@playwright/test";
+import { HomePage } from "../../src/pages/tredgate-eshop/home_page.ts";
+import { RegisterPage } from "../../src/pages/tredgate-eshop/register_page.ts";
+
+test("Register new account", async ({ page }) => {
+  const homePage = new HomePage(page);
+  const registerPage = new RegisterPage(page);
+
+  await homePage.open();
+  await homePage.clickMyAccount();
+  await homePage.clickRegister();
+  await registerPage.fillFirstName("Lucie");
+  await registerPage.fillLastName("Novakova");
+  await registerPage.fillEmail("test_new@tredgate.cz");
+  await registerPage.fillPhone("123456789");
+  await registerPage.fillPassword("Playwright321!");
+  await registerPage.fillPasswordConfirmation("Playwright321!");
+  await registerPage.clickPrivacyPolicy();
+  await registerPage.clickToRegister();
+});
